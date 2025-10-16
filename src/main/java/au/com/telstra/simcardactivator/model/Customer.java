3cab60644dc0ad3f5300b7fd60fb22939692294e
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2025-10-16 21:43:08 +0100

    feat: Added Customer Entity

---
+++ src/main/java/au/com/telstra/simcardactivator/model/Customer.java
@@ -0,0 +1,54 @@
+package au.com.telstra.simcardactivator.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String iccid;
+    private String customerEmail;
+    private boolean active;
+
+    protected Customer() {}
+
+    public Customer(String iccid, String customerEmail, boolean active) {
+        this.iccid = iccid;
+        this.customerEmail = customerEmail;
+        this.active = active;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getIccid() {
+        return iccid;
+    }
+
+    public void setIccid(String iccid) {
+        this.iccid = iccid;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
